c53dc6b77f87719d72ee7c0f2031da30d27a89c6
Arajdian Altaf <arajdianaltaf123@gmail.com>
2024-10-19 10:24:29 +0700

    deps: add mockito and netbeans configs

---
+++ src/test/java/com/test/HashTest.java
@@ -0,0 +1,96 @@
+package com.test;
+
+import com.util.HashUtil;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author pc
+ */
+public class HashTest {
+    private static HashUtil hashUtil;
+    
+    public HashTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+        hashUtil = new HashUtil();
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testHashNegativeCost() {
+        HashUtil hutil = new HashUtil(-1);
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testHashGreaterThan30Cost() {
+        HashUtil hutil = new HashUtil(31);
+    }
+    
+    @Test()
+    public void testHashCustomCost() {
+        int customCost = 3;
+        HashUtil hutil = new HashUtil(customCost);
+        String plainPassword = "verysecurepassword123";
+        String hash = hutil.hash(plainPassword);
+        
+        String[] expectedCostInHashStr = hash.split("\\$");
+        int usedCost = Integer.parseInt(expectedCostInHashStr[2]);
+        
+        assertEquals(usedCost, customCost);
+    }
+    
+    @Test(expected = NullPointerException.class)
+    public void testHashNullStringInput() {
+        String plainPassword = null;
+        String hash = hashUtil.hash(plainPassword);
+    }
+    
+    @Test
+    public void testHashAuthenticate() {
+        String hash = "$31$16$qAR5ChtrU7a1JxYrV9C9m4J6EpO9nu7_HdCazQ80Bz0";
+        String plainPassword = "oneofthemostsecurepasswordintheworld";
+        assertTrue(hashUtil.authenticate(plainPassword, hash));
+    }
+    
+    @Test
+    public void testHashWrongAuthenticate() {
+        String hash = "$31$16$qAR5ChtrU7a1JxYrV9C9m4J6EpO9nu7_HdCazQ80Bz0";
+        String plainPassword = "definitelytheincorrectpassword";
+        assertFalse(hashUtil.authenticate(plainPassword, hash));
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testHashAuthenticateInvalidHash() {
+        String hash = "";
+        String plainPassword = "oneofthemostsecurepasswordintheworld";
+        hashUtil.authenticate(plainPassword, hash);
+    }
+    
+    @Test 
+    public void testHashUnique() {
+        String plainPassword = "oneofthemostsecurepasswordintheworld";
+        String hash1 = hashUtil.hash(plainPassword);
+        String hash2 = hashUtil.hash(plainPassword);
+        
+        assertNotEquals(hash1, hash2);
+    }
+}
